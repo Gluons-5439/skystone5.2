5f7d91ad6635d5e8118643b2747ab7be60225590
BobbyJonesSr <clayhuang158@gmail.com>
2019-12-20 18:25:53 -0700

    Pushed code.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SetupProgram.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SetupProgram.java
@@ -13,35 +13,35 @@ public class SetupProgram extends LinearOpMode {
     double factor = 2;
 
     public void runOpMode() throws InterruptedException {
-        robot.init(hardwareMap, true);
-        AutonomousTools auto = new AutonomousTools();
-        //Upon initialization maps robot hardware
-
-        waitForStart();
-        while (opModeIsActive()) {
-            if (gamepad2.right_stick_x > .1) {
-                robot.bArmLeft.setPosition(robot.bArmLeft.getPosition() + .01);
-            } else if (gamepad2.right_stick_x < -.1) {
-                robot.bArmLeft.setPosition(robot.bArmLeft.getPosition() - .01);
-            }
-            telemetry.addData("Position of Servo", robot.bArmLeft.getPosition());
-            telemetry.addData("Position of Servo", robot.bArmRight.getPosition());
-            telemetry.update();
-            robot.waitForTick(40);
-
-            if (gamepad1.a) {
-                auto.openArms(robot);
-            }
-            if (gamepad1.b) {
-                auto.closeArms(robot);
-            }
-            if (gamepad1.dpad_left) {
-                robot.bArmLeft.setPosition(0);
-            }
-            if (gamepad1.dpad_right) {
-                robot.bArmRight.setPosition(0);
-            }
-        }
+//        robot.init(hardwareMap, true);
+//        AutonomousTools auto = new AutonomousTools();
+//        //Upon initialization maps robot hardware
+//
+//        waitForStart();
+//        while (opModeIsActive()) {
+//            if (gamepad2.right_stick_x > .1) {
+//                robot.bArmLeft.setPosition(robot.bArmLeft.getPosition() + .01);
+//            } else if (gamepad2.right_stick_x < -.1) {
+//                robot.bArmLeft.setPosition(robot.bArmLeft.getPosition() - .01);
+//            }
+//            telemetry.addData("Position of Servo", robot.bArmLeft.getPosition());
+//            telemetry.addData("Position of Servo", robot.bArmRight.getPosition());
+//            telemetry.update();
+//            robot.waitForTick(40);
+//
+//            if (gamepad1.a) {
+//                auto.openArms(robot);
+//            }
+//            if (gamepad1.b) {
+//                auto.closeArms(robot);
+//            }
+//            if (gamepad1.dpad_left) {
+//                robot.bArmLeft.setPosition(0);
+//            }
+//            if (gamepad1.dpad_right) {
+//                robot.bArmRight.setPosition(0);
+//            }
+//        }
 
         //Stops phone from queuing too many commands and breaking
     }
