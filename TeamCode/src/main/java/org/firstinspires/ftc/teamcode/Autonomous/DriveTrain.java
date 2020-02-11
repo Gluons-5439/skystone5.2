f61659a19596a6dfc9c6c23d4974bf52fee042eb
BobbyJonesSr <clayhuang158@gmail.com>
2020-02-11 15:57:01 -0800

    Refactored Hardware, separated DriveTrain into an abstract RobotMotors class and two different DriveTrain classes.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/DriveTrain.java
@@ -0,0 +1,57 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.Hardware.RobotMotors;
+
+public class DriveTrain extends RobotMotors {
+
+    public DriveTrain(DcMotor frontLeft, DcMotor frontRight, DcMotor backLeft, DcMotor backRight) {
+        super(frontLeft, frontRight, backLeft, backRight);
+    }
+
+    @Override
+    public void move(int inches, double power, int[] dir) {
+        for (int i = 0; i < 3; i++) {
+            wheels.get(i).setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            wheels.get(i).setTargetPosition(getTicks(inches) * dir[i]);
+            wheels.get(i).setPower(power * dir[i]);
+            wheels.get(i).setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        }
+
+        while (motorsAreBusy());
+
+        setMotorPower();
+    }
+
+    public void moveForward(int inches, double power) {
+        direction = MoveStyle.FORWARD;
+        move(inches, power, getDirs(direction));
+    }
+
+    public void moveBackward(int inches, double power) {
+        direction = MoveStyle.BACKWARD;
+        move(inches, power, getDirs(direction));
+    }
+
+    public void turnLeft(int inches, double power) {
+        direction = MoveStyle.TURN_LEFT;
+        move(inches, power, getDirs(direction));
+    }
+
+    public void turnRight(int inches, double power) {
+        direction = MoveStyle.TURN_RIGHT;
+        move(inches, power, getDirs(direction));
+    }
+
+    public void strafeLeft(int inches, double power) {
+        direction = MoveStyle.LEFT;
+        move(inches, power, getDirs(direction));
+    }
+
+    public void strafeRight(int inches, double power) {
+        direction = MoveStyle.BACKWARD;
+        move(inches, power, getDirs(direction));
+    }
+}
